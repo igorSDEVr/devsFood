fb25ab80411d267cc2c21ddf113bc27d43e93b1b
Igor Sousa <Igorsdev0509@gmail.com>
2022-12-24 10:20:41 -0300

    ccc

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -8,6 +8,10 @@ export const Container = styled.div`
     padding: 20px;
     border-radius: 10px;
 
+    @media (max-width: 750px) {
+        flex-direction: column;
+    }
+
     @media (max-width: 450px) {
         width: 250px;
     }
@@ -24,12 +28,12 @@ export const Logo = styled.img`
 
 export const SearchInput = styled.input<{active: boolean}>`
     height: 50px;
-    width: ${props => props.active ? '300' : '0'}px;
+    width: ${props => props.active ? '85%' : '0'};
     cursor: ${props => props.active ? 'text' : 'pointer'};
     font-size: 15px;
     background-color: #fff;
     padding-left: 50px;
-    transition: .3s;
+    transition: all .3s;
     border: 0;
     border-radius: 25px;
     outline: none;
@@ -37,6 +41,10 @@ export const SearchInput = styled.input<{active: boolean}>`
     background-size: 30px;
     background-repeat: no-repeat;
     background-position: 10px center;
+
+    @media (max-width: 750px) {
+        margin-top: 30px;
+    }
 
     @media (max-width: 450px) {
         margin-left: 10px;
